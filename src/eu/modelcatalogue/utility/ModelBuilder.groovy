46d1f8a194681d7272f1af25082035731586206f
David Milward <david@beenleigh.com>
2014-02-27 09:33:22 +0000

    added separate loader class

---
+++ src/eu/modelcatalogue/utility/ModelBuilder.groovy
@@ -0,0 +1,7 @@
+package eu.modelcatalogue.utility
+
+/**
+ * Created by davidmilward on 27/02/2014.
+ */
+class ModelBuilder {
+}
---
+++ test/eu/modelcatalogue/utility/MDRLoaderTest.groovy
@@ -0,0 +1,31 @@
+package eu.modelcatalogue.utility
+
+/**
+ * Created by davidmilward on 27/02/2014.
+ */
+class MDRLoaderTest extends GroovyTestCase {
+    void setUp() {
+        super.setUp()
+
+    }
+
+    void testSetSchemaAttributes() {
+
+    }
+
+    void testSetSimpleElementAttributes() {
+
+    }
+
+    void testSetAttributes() {
+
+    }
+
+    void testSetComplexAttributes() {
+
+    }
+
+    void testSetRestrictionAttributes() {
+
+    }
+}
