d64eab79f2744f25d76d5311ffc5fdf0c685e871
David Milward <david@beenleigh.com>
2014-03-03 22:59:00 +0000

    A few more tests added

---
+++ src/eu/modelcatalogue/utility/XMLArtefacts/XMLSimpleType.groovy
@@ -0,0 +1,7 @@
+package eu.modelcatalogue.utility.XMLArtefacts
+
+/**
+ * Created by davidmilward on 03/03/2014.
+ */
+class XMLSimpleType {
+}
---
+++ test/eu/modelcatalogue/utility/XMLArtefacts/XMLComplexElementTest.groovy
@@ -0,0 +1,147 @@
+package eu.modelcatalogue.utility.XMLArtefacts
+
+/**
+ * Created by davidmilward on 03/03/2014.
+ */
+class XMLComplexElementTest extends GroovyTestCase {
+    void setUp() {
+        super.setUp()
+
+    }
+
+    void testAddSchemaAttribute() {
+
+    }
+
+    void testGetSchemaAttribute() {
+
+    }
+
+    void testAddEnumItem() {
+
+    }
+
+    void testAddElement() {
+
+    }
+
+    void testAddRestriction() {
+
+    }
+
+    void testAddElementAttribute() {
+
+    }
+
+    void testGetRestrictionBase() {
+
+    }
+
+    void testSetRestrictionBase() {
+
+    }
+
+    void testGetbAbstract() {
+
+    }
+
+    void testIsbAbstract() {
+
+    }
+
+    void testSetbAbstract() {
+
+    }
+
+    void testGetbMixed() {
+
+    }
+
+    void testIsbMixed() {
+
+    }
+
+    void testSetbMixed() {
+
+    }
+
+    void testGetbFixed() {
+
+    }
+
+    void testIsbFixed() {
+
+    }
+
+    void testSetbFixed() {
+
+    }
+
+    void testGetbDefault() {
+
+    }
+
+    void testIsbDefault() {
+
+    }
+
+    void testSetbDefault() {
+
+    }
+
+    void testGetSchemaAttributeMap() {
+
+    }
+
+    void testSetSchemaAttributeMap() {
+
+    }
+
+    void testGetElementList() {
+
+    }
+
+    void testSetElementList() {
+
+    }
+
+    void testGetRestrictionList() {
+
+    }
+
+    void testSetRestrictionList() {
+
+    }
+
+    void testGetElementAttributeList() {
+
+    }
+
+    void testSetElementAttributeList() {
+
+    }
+
+    void testGetTypeEnumerations() {
+
+    }
+
+    void testSetTypeEnumerations() {
+
+    }
+
+    void testGetReference() {
+
+    }
+
+    void testSetReference() {
+
+    }
+
+    void testGetContainedElements() {
+
+    }
+
+    void testSetContainedElements() {
+
+    }
+}
