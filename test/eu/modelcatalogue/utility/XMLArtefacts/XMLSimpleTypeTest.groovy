0292d47152b3d9d4381560545f6fa2178457f403
David Milward <david@beenleigh.com>
2014-03-04 23:42:37 +0000

    Added a test for simpleTypes

---
+++ test/eu/modelcatalogue/utility/XMLArtefacts/XMLSimpleTypeTest.groovy
@@ -0,0 +1,35 @@
+package eu.modelcatalogue.utility.XMLArtefacts
+
+/**
+ * Created by davidmilward on 04/03/2014.
+ */
+class XMLSimpleTypeTest extends GroovyTestCase {
+    void setUp() {
+        super.setUp()
+
+    }
+
+    void testGetParentType() {
+
+    }
+
+    void testSetParentType() {
+
+    }
+
+    void testGetId() {
+
+    }
+
+    void testSetId() {
+
+    }
+
+    void testGetAttributes() {
+
+    }
+
+    void testSetAttributes() {
+
+    }
+}
