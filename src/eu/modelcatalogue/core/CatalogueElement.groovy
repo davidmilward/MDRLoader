b7513621881df7408db69522951838ce7cdb515c
David Milward <david.milward@cs.ox.ac.uk>
2014-03-17 16:22:49 +0000

    [#454]  test
    test askdjfalskdjhf

--- src/eu/modelcatalogue/core/CatalogueElement.groovy
+++ src/eu/modelcatalogue/core/CatalogueElement.groovy
@@ -1,6 +1,6 @@
 package eu.modelcatalogue.core
 
-//import grails.util.GrailsNameUtils
+//  import grails.util.GrailsNameUtils
 import org.apache.commons.lang.builder.EqualsBuilder
 import org.apache.commons.lang.builder.HashCodeBuilder
 
